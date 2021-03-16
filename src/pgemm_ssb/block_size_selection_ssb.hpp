e569509c261443b3a1866b880867f8f14a1e2c0a
Simon Frasch <simon.frasch@cscs.ch>
2021-03-16 17:52:08 +0100

    fix block size selection for non disjoint distribution

--- src/pgemm_ssb/block_size_selection_ssb.hpp
+++ src/pgemm_ssb/block_size_selection_ssb.hpp
@@ -141,7 +141,7 @@ inline auto block_size_selection_ssb(SplaFillMode mode, bool isDisjointDistribut
   std::tie(rowsInBlock, colsInBlock) = block_size_selection_ssb(
       isDisjointDistribution, commSize, m, n, targetBlockSize, deviationFactor, minBlockSize);
 
-  if (mode != SPLA_FILL_MODE_FULL) {
+  if (mode != SPLA_FILL_MODE_FULL && !isDisjointDistribution) {
     // For triangular case, the number of blocks that have to be computed is lower than the total ->
     // more blocks required for ring communication
     IntType numActiveBlocks = 0;
